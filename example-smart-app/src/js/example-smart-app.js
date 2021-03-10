c39fe943d367ad27e04172c4bac095e7b0e86a63
astevensgtri <79712019+astevensgtri@users.noreply.github.com>
2021-03-10 13:39:27 -0500

    Update example-smart-app.js
    Added Heart Rate from Obv

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -17,7 +17,8 @@
                       code: {
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4',
+                              'http://loinc.org|8867-4']
                       }
                     }
                   });
@@ -41,6 +42,7 @@
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
+          var hr = byCodes('8867-4');
 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
@@ -59,6 +61,7 @@
 
           p.hdl = getQuantityValueAndUnit(hdl[0]);
           p.ldl = getQuantityValueAndUnit(ldl[0]);
+          p.hr = getQuantityValueAndUnit(hr[0]);
 
           ret.resolve(p);
         });
@@ -83,6 +86,7 @@
       diastolicbp: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
+      hr: {value: ''}
     };
   }
 
@@ -126,6 +130,7 @@
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
+    $('#hr').html(p.hr);
   };
 
 })(window);
